bf0a64e928b62668f62ec3fb2a6c39183a9a5c62
bers <142059117+markvarkovi@users.noreply.github.com>
2023-11-01 22:21:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const input = `<input type="text" lang="en" id="input-text" placeholder="Write your text here">`;
+const addWordBtn = `<button id="submit-btn">SUBMIT</button>`;
+const resetBtn = `<button onClick="window.location.reload()" id="reset-btn">RESTART</button>`;
+const table = `<table id="word-table"></table>`;
+document.querySelector('#submit').insertAdjacentHTML('beforeend', addWordBtn);
+document.querySelector('#input').insertAdjacentHTML('beforeend', input);
+document.querySelector('#reset').insertAdjacentHTML('beforeend', resetBtn);
+document.querySelector('#table').insertAdjacentHTML('beforeend', table);
+
+const submitBtnElement = document.querySelector('#submit-btn');
+
+const tableElement = document.querySelector('#word-table');
+
+
+function makeTable() {
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let headerRow = "";
+    let textRows = ""
+    for (let i = 0; i < abc.length; i++) {
+        headerRow += `<th id="${abc[i]}">${abc[i]}${'&nbsp'}</th>`
+    }
+    for (let i = 0; i < abc.length; i++) {
+        textRows += `<td class="text-data" id="data${i}"></td>`
+    }
+    tableElement.insertAdjacentHTML('beforeend', `<tr class="header">${headerRow}</tr>`);
+    tableElement.insertAdjacentHTML('beforeend', `<tr class="rows" bgcolor="grey">${textRows}</tr>`);
+}
+
+function submitWord() {
+    const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const smallABC = abc.toLowerCase();
+
+    const inputElement = document.querySelector('#input-text')
+
+    submitBtnElement.addEventListener('click', function () {
+
+        let inputValue = inputElement.value
+
+        let index = 0;
+
+        // const A = document.querySelector('#data0').textContent
+        // if (A.length < 1) {
+        // if (A.charAt(A.length - 1) == inputValue.charAt(0)) {
+
+        //If the first letter is CORRECT
+        // if (!inputValue && data)
+        if (!smallABC.includes(inputValue.charAt(0)) && !abc.includes(inputValue.charAt(0))) { //First character invalid
+            alert("Invalid input! Special characters are not allowed!");
+            inputElement.value = "";
+            index = NaN;
+        } else if (!smallABC.includes(inputValue.charAt(inputValue.length - 1)) && !abc.includes(inputValue.charAt(inputValue.length - 1))) {
+            alert("Invalid input! Special characters are not allowed!"); //Last character invalid
+            inputElement.value = "";
+            index = NaN;
+        }
+        else if (abc.includes(inputValue.charAt(0))) index = abc.indexOf(inputValue.charAt(0)); //Capital letter to be added under the correct letter
+        else if (smallABC.includes(inputValue.charAt(0))) index = smallABC.indexOf(inputValue.charAt(0)); //Underscore letter to be added under the correct letter
+        else { // If the first letter is not the previous word's last letter
+            alert("Incorrect word! You can only start your word with the previous word's last letter!");
+            inputElement.value = "";
+            index = NaN;
+        }
+        
+        const tableDataElement = document.querySelector(`#data${index}`)
+
+        tableDataElement.insertAdjacentHTML('beforeend', inputValue)
+
+        inputElement.value = "";
+    })
+}
+makeTable();
+submitWord();
+
+const data = document.querySelectorAll('.text-data')
+let container = "";
+for (const element of data) 
+    container = element
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,9 @@
+let abc = "ABCDEFGH"
+let smallAbc =  abc.toLowerCase();
+let index = 0;
+let word;
+
+
+let splitAbc = abc.split("")
+
+console.log(word.length);
